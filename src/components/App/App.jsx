8bc036d72a7225581ed8848bbc7b97ba46b56eef
Robertw8 <oniedgaf@gmail.com>
2023-09-08 19:13:59 +0300

    done App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,69 @@
 import React, { Component } from "react";
-import { Global } from "@emotion/react";
-import { ContactList } from "../ContactList/ContactList";
 import { ContactForm } from "../ContactForm/ContactForm";
+import { ContactList } from "../ContactList/ContactList";
+import { Filter } from "../Filter/Filter";
 
+import { Global } from "@emotion/react";
 import { globalStyles } from "../../styles/globalStyles";
+import { ContactsWrapper, Title } from "./App.styled";
+
+import Notiflix from "notiflix";
+import { nanoid } from "nanoid";
 
 export class App extends Component {
-	state = {
-		contacts: [],
-		name: "",
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			contacts: [
+				{ id: nanoid(), name: "Rosie Simpson", number: "459-12-56" },
+				{ id: nanoid(), name: "Hermione Kline", number: "443-89-12" },
+			],
+			filter: "",
+		};
+	}
+
+	handleSubmit = (newContact) => {
+		const isDuplicate = this.state.contacts.some(
+			(contact) => contact.name.toLowerCase() === newContact.name.toLowerCase() || contact.number === newContact.number,
+		);
+
+		if (isDuplicate) {
+			Notiflix.Notify.info("This contact already exists ");
+			return;
+		}
+
+		this.setState((prev) => ({
+			contacts: [...prev.contacts, newContact],
+		}));
+	};
+
+	handleDelete = (id) => {
+		this.setState((prevState) => ({
+			contacts: prevState.contacts.filter((contact) => contact.id !== id),
+		}));
+	};
+
+	handleFilterChange = (e) => {
+		const filter = e.target.value;
+		this.setState({ filter });
 	};
 
 	render() {
+		const { contacts, filter } = this.state;
+		const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
+
 		return (
-			<>
+			<div className='container'>
 				<Global styles={globalStyles} />
-				<ContactForm />
-				<ContactList />
-			</>
+				<Title>Phonebook</Title>
+				<ContactForm onSubmit={this.handleSubmit} />
+				<ContactsWrapper>
+					<Title>Contacts</Title>
+					<Filter filter={filter} onChange={this.handleFilterChange} />
+					<ContactList contacts={filteredContacts} onDelete={this.handleDelete} />
+				</ContactsWrapper>
+			</div>
 		);
 	}
 }
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const ContactsWrapper = styled.div`
+	margin-top: 100px;
+`;
+
+const Title = styled.h2`
+	font-size: 20px;
+	color: #333;
+	margin-bottom: 20px;
+`;
+
+export { ContactsWrapper, Title };
