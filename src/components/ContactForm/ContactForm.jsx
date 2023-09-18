d0192fd05e368e9f0d54cf768b6e63d291c1534e
Robertw8 <oniedgaf@gmail.com>
2023-09-18 17:37:12 +0300

    refactored ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,73 +1,83 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
-
+import * as yup from "yup";
 import { Form } from "./ContactForm.styled";
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
 
-export class ContactForm extends Component {
-	state = {
-		name: "",
-		number: "",
-	};
+const schema = yup.object().shape({
+	name: yup
+		.string()
+		.required("Name is required")
+		.matches(/^[a-zA-Z-' ]+$/, "Invalid name format"),
+	number: yup
+		.string()
+		.required("Phone number is required")
+		.matches(/^[0-9+()-\s]+$/, "Invalid phone number format"),
+});
 
-	handleSubmit = (e) => {
+export const ContactForm = ({ onSubmit }) => {
+	const [name, setName] = useState("");
+	const [number, setNumber] = useState("");
+	const [errors, setErrors] = useState({});
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		const { name, number } = this.state;
 
-		if (name.trim() === "" || number.trim() === "") {
-			return;
+		try {
+			await schema.validate({ name, number }, { abortEarly: false });
+
+			const newContact = {
+				id: nanoid(),
+				name,
+				number,
+			};
+
+			onSubmit(newContact);
+
+			setName("");
+			setNumber("");
+			setErrors({});
+		} catch (err) {
+			const validationErrors = {};
+			err.inner.forEach((e) => {
+				validationErrors[e.path] = e.message;
+			});
+			setErrors(validationErrors);
 		}
-
-		const newContact = {
-			id: nanoid(),
-			name,
-			number,
-		};
-
-		this.props.onSubmit(newContact);
-		this.setState({
-			name: "",
-			number: "",
-		});
 	};
 
-	handleInputChange = (e) => {
-		const { name, value } = e.target;
-		this.setState({ [name]: value });
-	};
-
-	render() {
-		return (
-			<>
-				<Form name='contact' onSubmit={this.handleSubmit}>
-					<TextField
-						id='outlined'
-						label='Name'
-						type='text'
-						name='name'
-						value={this.state.name}
-						onChange={this.handleInputChange}
-						title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-						placeholder='Enter name...'
-						required
-					/>
-					<TextField
-						id='outlined'
-						label='Phone'
-						type='tel'
-						name='number'
-						value={this.state.number}
-						onChange={this.handleInputChange}
-						title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-						placeholder='Enter phone number...'
-						required
-					/>
-					<Button variant='contained' color='info' type='submit' style={{ width: "40%", alignSelf: "flex-end" }}>
-						Add contact
-					</Button>
-				</Form>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<Form name='contact' onSubmit={handleSubmit}>
+				<TextField
+					id='outlined'
+					label='Name'
+					type='text'
+					name='name'
+					placeholder='Enter name...'
+					required
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					error={!!errors.name}
+					helperText={errors.name}
+				/>
+				<TextField
+					id='outlined'
+					label='Phone'
+					type='tel'
+					name='number'
+					placeholder='Enter phone number...'
+					required
+					value={number}
+					onChange={(e) => setNumber(e.target.value)}
+					error={!!errors.number}
+					helperText={errors.number}
+				/>
+				<Button variant='contained' color='info' type='submit' style={{ width: "40%", alignSelf: "flex-end" }}>
+					Add contact
+				</Button>
+			</Form>
+		</>
+	);
+};
