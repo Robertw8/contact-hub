5b1c5a2312e4d68972f069959c73959d566e36c3
Robertw8 <oniedgaf@gmail.com>
2023-09-18 17:37:20 +0300

    refactored ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { List, Item } from "./ContactList.styled";
 import { nanoid } from "nanoid";
 
-export class ContactList extends Component {
-	componentDidUpdate(prevProps, prevState) {
-		localStorage.setItem("contacts", JSON.stringify(this.props.contacts));
-	}
+export const ContactList = ({ contacts, onDelete, updateContacts }) => {
+	useEffect(() => {
+		localStorage.setItem("contacts", JSON.stringify(contacts));
+	}, [contacts]);
 
-	render() {
-		const { contacts, onDelete } = this.props;
-
-		return (
-			<List>
-				{contacts.map((item) => (
-					<Item key={nanoid()}>
-						{item.name} ({item.number})
-						<IconButton aria-label='delete' size='medium' onClick={() => onDelete(item.id)}>
-							<DeleteIcon color='warning' />
-						</IconButton>
-					</Item>
-				))}
-			</List>
-		);
-	}
-}
+	return (
+		<List>
+			{contacts.map((contact) => (
+				<Item key={nanoid()}>
+					{contact.name} ({contact.number})
+					<IconButton aria-label='delete' size='medium' onClick={() => onDelete(contact.id)}>
+						<DeleteIcon color='warning' />
+					</IconButton>
+				</Item>
+			))}
+		</List>
+	);
+};
