4ce296e2dc95e1e0f60c00b1f1b25f4b09d39cd2
Robertw8 <oniedgaf@gmail.com>
2023-10-03 04:45:39 +0300

    clean up App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { ContactForm } from "../ContactForm/ContactForm";
 import { ContactList } from "../ContactList/ContactList";
 import { Filter } from "../Filter/Filter";
-
 import { Global } from "@emotion/react";
 import { globalStyles } from "../../styles/globalStyles";
 import { ContactsWrapper, Title } from "./App.styled";
 
-import Notiflix from "notiflix";
-
 export const App = () => {
-	const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem("contacts")) || []);
-	const [filter, setFilter] = useState("");
-
-	const handleSubmit = (newContact) => {
-		const isDuplicate = contacts.find(
-			(contact) => contact.name.toLowerCase() === newContact.name.toLowerCase() || contact.number === newContact.number,
-		);
-
-		if (isDuplicate) {
-			Notiflix.Notify.info("This contact already exists ");
-			return;
-		}
-
-		setContacts((prevContacts) => [...prevContacts, newContact]);
-	};
-
-	const handleDelete = (id) => {
-		setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
-	};
-
-	const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
-
 	return (
 		<div className='container'>
 			<Global styles={globalStyles} />
 			<Title>Phonebook</Title>
-			<ContactForm onSubmit={handleSubmit} />
+			<ContactForm />
 			<ContactsWrapper>
 				<Title>Contacts</Title>
-				<Filter filter={filter} onChange={(e) => setFilter(e.target.value)} />
-				<ContactList contacts={filteredContacts} onDelete={handleDelete} />
+				<Filter />
+				<ContactList />
 			</ContactsWrapper>
 		</div>
 	);
