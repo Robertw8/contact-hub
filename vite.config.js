7d8d231bbb0b935c8793eb8e31e20702dd521e4c
Robertw8 <oniedgaf@gmail.com>
2023-10-17 03:11:22 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
 	base: "/goit-react-hw-08-phonebook/",
+	plugins: [react()],
 });
