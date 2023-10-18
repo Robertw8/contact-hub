0d8f14e5963c28310d4ac68e8659297bfd2e44ba
Robertw8 <oniedgaf@gmail.com>
2023-10-19 01:04:37 +0300

    refactored contactsSlice

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,24 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { contactsApi } from "./operations";
 
 const contactsSlice = createSlice({
 	name: "contacts",
-	initialState: contactsApi.endpoints.getContacts.select(),
-	reducers: {},
-	extraReducers: (builder) => {
-		builder
-			.addMatcher(contactsApi.util.isAnyOf(getContacts, addContact, deleteContact).pending, (state) => {
-				state.isLoading = true;
-				state.error = null;
-			})
-			.addMatcher(contactsApi.util.isAnyOf(getContacts, addContact, deleteContact).fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.contacts = action.payload;
-			})
-			.addMatcher(contactsApi.util.isAnyOf(getContacts, addContact, deleteContact).rejected, (state, action) => {
-				state.isLoading = false;
-				state.error = action.error.message;
-			});
+	initialState: {
+		contacts: [],
+	},
+	reducers: {
+		addContact: (state, action) => {
+			state.contacts.push(action.payload);
+		},
+		deleteContact: (state, action) => {
+			state.contacts.filter((contact) => contact.id !== action.payload.id);
+		},
 	},
 });
 
