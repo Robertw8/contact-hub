558193963a879163e50888753dcf7bd76a8f4d7b
Robertw8 <oniedgaf@gmail.com>
2023-10-24 02:13:42 +0300

    add auth selectors

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,4 @@
+export const selectToken = (state) => state.auth.token;
+export const selectUser = (state) => state.auth.user;
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
