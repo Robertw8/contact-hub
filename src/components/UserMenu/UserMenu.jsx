22d617d83cc41324e0546dd9b5c31bcd8ac647da
Robertw8 <oniedgaf@gmail.com>
2023-10-24 02:44:34 +0300

    add user name

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/auth/operations";
+import { useSelector } from "react-redux";
+import { selectUser, selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export const UserMenu = () => {
 	const dispatch = useDispatch();
+	const user = useSelector(selectUser);
+	const isLoggedIn = useSelector(selectIsLoggedIn);
 
 	const handleLogoutClick = () => {
 		dispatch(logout());
@@ -11,12 +15,12 @@ export const UserMenu = () => {
 
 	return (
 		<div>
-			{/* {isLoading && <p>Loading...</p>}
-			{error && <p>Error: {error.message}</p>}
-			{currentUser && <p>Email: {currentUser.email}</p>} */}
-			<button type='button' onClick={handleLogoutClick}>
-				Logout
-			</button>
+			<p>Welcome, {user.name}!</p>
+			{isLoggedIn && (
+				<button type='button' onClick={handleLogoutClick}>
+					Logout
+				</button>
+			)}
 		</div>
 	);
 };
