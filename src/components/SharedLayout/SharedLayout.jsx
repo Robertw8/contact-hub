042ed2938d0a932cfff68c224ce6af37480ebeb8
Robertw8 <oniedgaf@gmail.com>
2023-10-25 02:56:38 +0300

    change navbar to usermenu

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,20 @@
 import React, { Suspense } from "react";
-import { NavBar } from "../NavBar/NavBar";
 import { Outlet } from "react-router-dom";
-import { HashLoader } from "react-spinners";
+import { Container } from "../../styles/globalStyles";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { UserMenu } from "../UserMenu/UserMenu";
+import { Loader } from "../Loader/Loader";
 
 export const SharedLayout = () => {
+	const isLoggedIn = useSelector(selectIsLoggedIn);
+
 	return (
-		<>
-			<NavBar />
-			<Suspense
-				fallback={
-					<div>
-						<HashLoader />
-					</div>
-				}
-			>
+		<Container>
+			{isLoggedIn && <UserMenu />}
+			<Suspense fallback={<Loader />}>
 				<Outlet />
 			</Suspense>
-		</>
+		</Container>
 	);
 };
