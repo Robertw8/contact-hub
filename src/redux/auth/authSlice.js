d0ca464a689be82f23f175b4194e81a542a68186
Robertw8 <oniedgaf@gmail.com>
2023-10-26 23:36:44 +0300

    update formatting

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -4,81 +4,84 @@ import storage from "redux-persist/lib/storage";
 import { signup, login, logout, currentUser } from "./operations";
 
 const authConfig = {
-	key: "auth",
-	storage,
-	whitelist: ["token"],
+  key: "auth",
+  storage,
+  whitelist: ["token"],
 };
 
 const initialState = {
-	user: { name: null, email: null },
-	token: null,
-	isLoggedIn: false,
-	isRefreshing: false,
-	isLoading: false,
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  isLoading: false,
 };
 
-const handlePending = (state) => {
-	state.isLoading = true;
+const handlePending = state => {
+  state.isLoading = true;
 };
 
-const handleRejected = (state) => {
-	state.isLoading = false;
+const handleRejected = state => {
+  state.isLoading = false;
 };
 
 const authSlice = createSlice({
-	name: "auth",
-	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		builder
-			.addCase(signup.fulfilled, (state, action) => {
-				state.user = action.payload.user;
-				state.token = action.payload.token;
-				state.isLoggedIn = true;
-				state.isLoading = false;
-			})
-			.addCase(signup.pending, (state) => {
-				handlePending(state);
-			})
-			.addCase(signup.rejected, (state) => {
-				handleRejected(state);
-			})
-			.addCase(login.fulfilled, (state, action) => {
-				state.user = action.payload.user;
-				state.token = action.payload.token;
-				state.isLoggedIn = true;
-				state.isLoading = false;
-			})
-			.addCase(login.pending, (state) => {
-				handlePending(state);
-			})
-			.addCase(login.rejected, (state) => {
-				handleRejected(state);
-			})
-			.addCase(logout.fulfilled, (state) => {
-				state.user = { name: null, email: null };
-				state.token = null;
-				state.isLoggedIn = false;
-				state.isLoading = false;
-			})
-			.addCase(logout.pending, (state) => {
-				handlePending(state);
-			})
-			.addCase(logout.rejected, (state) => {
-				handleRejected(state);
-			})
-			.addCase(currentUser.pending, (state) => {
-				state.isRefreshing = true;
-			})
-			.addCase(currentUser.fulfilled, (state, action) => {
-				state.user = action.payload;
-				state.isLoggedIn = true;
-				state.isRefreshing = false;
-			})
-			.addCase(currentUser.rejected, (state) => {
-				state.isRefreshing = false;
-			});
-	},
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(signup.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+      })
+      .addCase(signup.pending, state => {
+        handlePending(state);
+      })
+      .addCase(signup.rejected, state => {
+        handleRejected(state);
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+      })
+      .addCase(login.pending, state => {
+        handlePending(state);
+      })
+      .addCase(login.rejected, state => {
+        handleRejected(state);
+      })
+      .addCase(logout.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isLoading = false;
+      })
+      .addCase(logout.pending, state => {
+        handlePending(state);
+      })
+      .addCase(logout.rejected, state => {
+        handleRejected(state);
+      })
+      .addCase(currentUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(currentUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(currentUser.rejected, state => {
+        state.isRefreshing = false;
+      });
+  },
 });
 
-export const persistedAuthReducer = persistReducer(authConfig, authSlice.reducer);
+export const persistedAuthReducer = persistReducer(
+  authConfig,
+  authSlice.reducer
+);
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,63 +1,71 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { errorToast } from "../../utils/toast";
 
 axios.defaults.baseURL = "https://connections-api.herokuapp.com/";
 
-const setHeader = (token) => {
-	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+const setHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearHeader = () => {
-	axios.defaults.headers.common.Authorization = null;
+  axios.defaults.headers.common.Authorization = null;
 };
 
-const signup = createAsyncThunk("auth/signup", async (credentials, thunkAPI) => {
-	try {
-		const response = await axios.post("users/signup", credentials);
+const signup = createAsyncThunk(
+  "auth/signup",
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post("users/signup", credentials);
 
-		setHeader(response.data.token);
-		return response.data;
-	} catch (error) {
-		thunkAPI.rejectWithValue(error.message);
-	}
-});
+      setHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 const login = createAsyncThunk("auth/login", async (credentials, thunkAPI) => {
-	try {
-		const response = await axios.post("users/login", credentials);
+  try {
+    const response = await axios.post("users/login", credentials);
 
-		setHeader(response.data.token);
-		return response.data;
-	} catch (error) {
-		thunkAPI.rejectWithValue(error.message);
-	}
+    setHeader(response.data.token);
+    return response.data;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error.message);
+  }
 });
 
 const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
-	try {
-		const response = await axios.post("users/logout");
+  try {
+    const response = await axios.post("users/logout");
 
-		clearHeader();
-		return response.data;
-	} catch (error) {
-		thunkAPI.rejectWithValue(error.message);
-	}
+    clearHeader();
+    return response.data;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error.message);
+  }
 });
 
-const currentUser = createAsyncThunk("auth/currentUser", async (_, thunkAPI) => {
-	const state = thunkAPI.getState();
-	const persistedToken = state.auth.token;
+const currentUser = createAsyncThunk(
+  "auth/currentUser",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
 
-	if (!persistedToken) return thunkAPI.rejectWithValue("Unable to fetch user");
+    if (!persistedToken)
+      return thunkAPI.rejectWithValue("Unable to fetch user");
 
-	try {
-		setHeader(persistedToken);
-		const response = await axios.get("users/current");
+    try {
+      setHeader(persistedToken);
+      const response = await axios.get("users/current");
 
-		return response.data;
-	} catch (error) {
-		return thunkAPI.rejectWithValue(error.message);
-	}
-});
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 export { signup, login, logout, currentUser };
