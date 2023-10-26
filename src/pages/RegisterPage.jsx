6605f22de19c8da43f691cc042ae9386d53b7da6
Robertw8 <oniedgaf@gmail.com>
2023-10-26 20:35:41 +0300

    refactored RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,48 +1,25 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { signup } from "../redux/auth/operations";
-import { useToken } from "../hooks/useToken";
-import { Link } from "react-router-dom";
 import { Background } from "../components/Background/Background";
 import { RegisterForm } from "../components/RegisterForm/RegisterForm";
+import { useAuth } from "../hooks/useAuth";
 
 const RegisterPage = () => {
-	const navigate = useNavigate();
-	const TOKEN = useToken();
-	const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
 
-	useEffect(() => {
-		if (TOKEN) navigate("/");
-		return;
-	}, [TOKEN, navigate]);
+  useEffect(() => {
+    isLoggedIn && navigate("/contacts");
+  }, [isLoggedIn, navigate]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-
-		const userData = {
-			name: e.target.elements[0].value,
-			email: e.target.elements[1].value,
-			password: e.target.elements[2].value,
-		};
-
-		try {
-			const response = await dispatch(signup(userData));
-
-			return response.data;
-		} catch (error) {
-			console.warn("Registration failed: ", error);
-		}
-
-		e.target.reset();
-	};
-
-	return (
-		<>
-			<Background />
-			<RegisterForm />
-		</>
-	);
+  return (
+    !isLoggedIn && (
+      <>
+        <Background />
+        <RegisterForm />
+      </>
+    )
+  );
 };
 
 export default RegisterPage;
