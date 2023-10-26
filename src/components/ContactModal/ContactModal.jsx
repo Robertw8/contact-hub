62c458af97e0ce93d3ab955007074603511f0076
Robertw8 <oniedgaf@gmail.com>
2023-10-26 20:37:24 +0300

    remove contact modal

--- src/components/ContactModal/ContactModal.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { ContactForm } from "../ContactForm/ContactForm";
-import { Modal } from "antd";
-
-export const ContactModal = ({ isOpen }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return <Modal open={isModalOpen} onCancel={() => setIsModalOpen(false)} />;
-};
