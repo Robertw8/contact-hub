0b734b4b652c3698858e4fd219f10033b2335727
Robertw8 <oniedgaf@gmail.com>
2023-10-26 03:40:17 +0300

    restyle ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,46 +1,82 @@
-import React, { useEffect, useId } from "react";
-import { List, Item } from "./ContactList.styled";
+import React, { useEffect, useState } from "react";
+import {
+  List,
+  Item,
+  DeleteButton,
+  DeleteIcon,
+  AddButton,
+  AddItem,
+} from "./ContactList.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact, getContacts } from "../../redux/contacts/operations";
-import { selectContacts, selectIsLoading } from "../../redux/contacts/selectors";
+import {
+  selectContacts,
+  selectIsLoading,
+} from "../../redux/contacts/selectors";
 import { selectFilter } from "../../redux/filter/filterSlice";
-import { HashLoader } from "react-spinners";
+import { Card, Button, Skeleton } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { ContactModal } from "../ContactModal/ContactModal";
 
 export const ContactList = () => {
-	const dispatch = useDispatch();
-	const contacts = useSelector(selectContacts).contacts;
-	// const id = useId();
-	const filter = useSelector(selectFilter);
-	const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+  const isLoading = useSelector(selectIsLoading);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	useEffect(() => {
-		dispatch(getContacts());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
 
-	const handleDeleteClick = (contactId) => {
-		dispatch(deleteContact(contactId));
-	};
+  const handleDeleteClick = contactId => {
+    dispatch(deleteContact(contactId));
+  };
 
-	const filteredContacts = contacts.filter(
-		(contact) => contact.name.toLowerCase().includes(filter.toLowerCase()) || contact.number.includes(filter),
-	);
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  );
 
-	return (
-		<List>
-			{filteredContacts ? (
-				isLoading ? (
-					<HashLoader />
-				) : (
-					filteredContacts.map((contact) => (
-						<Item key={contact.id}>
-							{contact.name} ({contact.number})
-							<button type='button' onClick={() => handleDeleteClick(contact.id)}>
-								Delete
-							</button>
-						</Item>
-					))
-				)
-			) : null}
-		</List>
-	);
+  return (
+    <>
+      <List>
+        {filteredContacts.map(contact => (
+          <Item
+            key={contact.id}
+            actions={[
+              <DeleteButton
+                key="delete"
+                type="dashed"
+                onClick={() => handleDeleteClick(contact.id)}
+                htmlType="button"
+                aria-label="delete contact"
+                loading={isLoading}
+              >
+                <DeleteIcon />
+              </DeleteButton>,
+              <Button
+                key="edit"
+                type="dashed"
+                htmlType="button"
+                aria-label="edit contact"
+                loading={isLoading}
+              >
+                <EditOutlined />
+              </Button>,
+            ]}
+          >
+            <Card.Meta title={contact.name} description={contact.number} />
+          </Item>
+        ))}
+        <AddItem>
+          <AddButton type="primary" onClick={() => setIsModalOpen(true)}>
+            Add +
+          </AddButton>
+        </AddItem>
+      </List>
+      <ContactModal isOpen={isModalOpen} />
+    </>
+  );
 };
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -1,29 +1,62 @@
 import styled from "@emotion/styled";
 import { HashLoader } from "react-spinners";
+import { DeleteTwoTone } from "@ant-design/icons";
+import { Button, Card } from "antd";
 
-const List = styled.ul`
-	position: relative;
-	display: flex;
-	flex-wrap: wrap;
-	gap: 40px;
+const List = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 40px;
+  padding: 100px;
+
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  border: 1px solid #f9f9f9;
 `;
 
-const Item = styled.li`
-	margin-top: 20px;
-	width: 300px;
-	padding: 10px;
-
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	box-shadow: 0px 2px 4px 2px #33333333;
+const Item = styled(Card)`
+  width: 300px;
 `;
+
+const AddItem = styled.li`
+  position: absolute;
+  bottom: 3%;
+
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const DeleteButton = styled(Button)``;
+
+const DeleteIcon = styled(DeleteTwoTone)``;
 
 const StyledLoader = styled(HashLoader)`
-	position: absolute;
-	left: 50%;
-	transform: translateY(200%) !important;
+  position: absolute;
+  left: 50%;
+  transform: translateY(200%) !important;
 `;
 
-export { List, Item, StyledLoader };
+const AddButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 16px;
+  width: 150px;
+  height: 50px;
+
+  background-color: #1677ff;
+`;
+
+export {
+  List,
+  Item,
+  StyledLoader,
+  DeleteButton,
+  DeleteIcon,
+  AddButton,
+  AddItem,
+};
