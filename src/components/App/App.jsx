84ef94d6d021a36733812bcd76a76853a004672f
Robertw8 <oniedgaf@gmail.com>
2023-10-26 20:35:22 +0300

    make new route for a contacts page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { SharedLayout } from "../SharedLayout/SharedLayout";
 import { useDispatch } from "react-redux";
 import { currentUser } from "../../redux/auth/operations";
-// import { RestrictedRoute } from "../RestrictedRoute";
-// import { PrivateRoute } from "../PrivateRoute";
 
 const HomePage = lazy(() => import("../../pages/HomePage"));
 const RegisterPage = lazy(() => import("../../pages/RegisterPage"));
 const LoginPage = lazy(() => import("../../pages/LoginPage"));
+const ContactsPage = lazy(() => import("../../pages/ContactsPage"));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -26,6 +25,7 @@ export const App = () => {
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
+          <Route path="contacts" element={<ContactsPage />} />
         </Route>
       </Routes>
     </>
