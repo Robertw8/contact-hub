3bc37a71f1ac37ecc58efc27c3e97bcc269cacd3
Robertw8 <Robertw8@users.noreply.github.com>
2023-10-26 17:39:11 +0000

    deploy: 18cd6b9c29f0089b0cc28c9909f16347696217fe

---
+++ assets/HomePage-4f034389.js
@@ -0,0 +1,22 @@
+import{n,B as c,u as i,j as e,a as r,r as a}from"./index-08461a00.js";import{B as l}from"./Background-57147280.js";const x=n.div`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	padding: 20% 0;
+	user-select: none;
+`,p=n.div`
+	display: flex;
+	gap: 20px;
+	justify-content: center;
+	margin-top: 50px;
+`,d=n.p`
+	color: #fff;
+	text-align: center;
+	font-size: 48px;
+	z-index: 10;
+`,o=n(c)`
+	width: 150px;
+	height: 60px;
+
+	font-size: 18px;
+`,u=()=>{const t=i();return e.jsxs(x,{children:[e.jsx(l,{}),e.jsx(d,{children:"Welcome to ContactHub!"}),e.jsxs(p,{children:[e.jsx(o,{type:"primary",onClick:()=>t("/register"),children:"Register"}),e.jsx(o,{type:"default",onClick:()=>t("/login"),children:"Login"})]})]})},h=()=>{const t=i(),{isLoggedIn:s}=r();return a.useEffect(()=>{s&&t("/contacts")},[s,t]),!s&&e.jsx(u,{})};export{h as default};
--- assets/useToken-f824dfde.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e,ab as o}from"./index-c0ecfaa2.js";const r=()=>e(o);export{r as u};
