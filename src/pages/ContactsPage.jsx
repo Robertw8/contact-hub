e83161d431833a0fad5a826430296fb29cd57094
Robertw8 <oniedgaf@gmail.com>
2023-10-28 01:39:22 +0300

    change render condition

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { ContactWrapper } from "../components/ContactWrapper/ContactWrapper";
+import { Loader } from "../components/Loader/Loader";
 
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 const ContactsPage = () => {
   const navigate = useNavigate();
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isLoading, isRefreshing } = useAuth();
 
   useEffect(() => {
     !isLoggedIn && navigate("/");
   }, [isLoggedIn, navigate]);
 
-  return isLoggedIn && <ContactWrapper />;
+  return !isLoading ? <ContactWrapper /> : <Loader />;
 };
 
 export default ContactsPage;
