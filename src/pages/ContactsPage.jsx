db950c0094f6b0cd7bacf575b0374f43b6678e97
Robertw8 <oniedgaf@gmail.com>
2023-10-28 01:46:14 +0300

    revert render conditions on contacts page

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { ContactWrapper } from "../components/ContactWrapper/ContactWrapper";
-import { Loader } from "../components/Loader/Loader";
 
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 const ContactsPage = () => {
   const navigate = useNavigate();
-  const { isLoggedIn, isLoading, isRefreshing } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   useEffect(() => {
     !isLoggedIn && navigate("/");
   }, [isLoggedIn, navigate]);
 
-  return !isLoading ? <ContactWrapper /> : <Loader />;
+  return isLoggedIn && <ContactWrapper />;
 };
 
 export default ContactsPage;
